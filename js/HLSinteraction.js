1250e8e27a993bcd7a575772dacc47376407a235
Richard Felix <rfelixmail@gmail.com>
2015-07-06 15:11:29 -0400

    drill down work

--- js/HLSinteraction.js
+++ js/HLSinteraction.js
@@ -3,57 +3,48 @@ function onclickcall(evt){
     var deleteChartDivs = evt.parentNode.parentNode.parentNode.parentNode;
     var chartDiv = deleteChartDivs.parentNode.getAttribute('id');
     var graphType = evt.parentNode.parentNode.getAttribute('id');
+    var theme = evt.getAttribute('theme');
+    var height = evt.parentNode.parentNode.getAttribute('height');
+    var width = evt.parentNode.parentNode.getAttribute('width');
+    var xColumn = evt.getAttribute('xcolumn');
+    var yColumn = evt.getAttribute('ycolumn');
+    yColumn = "["+yColumn+"]"
     deleteChartDivs.parentNode.removeChild(deleteChartDivs);  
 
-    drawNewChart(chartDiv, graphType);
+    drawNewChart(chartDiv, width, height, xColumn, yColumn, graphType, theme);
 }
 
-function drawNewChart(chartDiv, graphType){
-
+function drawNewChart(chartDiv, width, height, xColumn, yColumn, graphType, theme){
+var filename = 'data/jan/jan.json';
     var controllerDiv = document.createElement( graphType + 'ControllerDiv' );
         controllerDiv.setAttribute('id', chartDiv + 'controllerDiv');
         controllerDiv.setAttribute('ng-controller', "myController");
         document.getElementById(chartDiv).appendChild(controllerDiv);
     
-//    var chart = document.createElement(graphType);
-//    
-//        if( graphType == 'linechart' || graphType == 'scatterchart' ){
-//            chart.setAttribute('logview', logview);
-//            if(height == undefined)
-//                height = 500;
-//            if(width == undefined)
-//                width = 500;
-//        
-//        }else{
-//            
-//            if(height == undefined)
-//                height = 500;
-//            if( width == undefined && graphType == 'stackchart' )
-//                width == 500;
-//        }
-//    
-//        chart.setAttribute('id', graphType + counter );
-//        chart.setAttribute('filename', filename);
-//        chart.setAttribute('xColumn', xColumn);
-//        chart.setAttribute('yColumn', yColumn);
-//        chart.setAttribute('width', width);
-//        chart.setAttribute('height', height);
-//        chart.setAttribute('theme', theme);
-//    
-//        document.getElementById( graphType + 'controllerDiv' + counter).appendChild(chart);
-//}
-//
-//
-//function themeSelection(theme){
-//    
-//    if(theme == 'dark')
-//        theme = 1;
-//    else if(theme == 'light')
-//        theme = 2;
-//    else if(theme == 'neon')
-//        theme = 3;
-//    else
-//        theme = 0;
-//    
-//    return theme;
+    var chart = document.createElement(graphType);
+    
+        if( graphType == 'linechart' || graphType == 'scatterchart' ){
+            chart.setAttribute('logview', logview);
+            if(height == undefined)
+                height = 500;
+            if(width == undefined)
+                width = 500;
+        
+        }else{
+            
+            if(height == undefined)
+                height = 500;
+            if( width == undefined && graphType == 'stackchart' )
+                width == 500;
+        }
+    
+        chart.setAttribute('id', graphType + chartDiv );
+        chart.setAttribute('filename', filename);
+        chart.setAttribute('xColumn', xColumn);
+        chart.setAttribute('yColumn', yColumn);
+        chart.setAttribute('width', width);
+        chart.setAttribute('height', height);
+        chart.setAttribute('theme', theme);
+    
+        document.getElementById(chartDiv + 'controllerDiv').appendChild(chart);
 }
